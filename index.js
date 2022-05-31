5e82b3c5fae2c429ea391454b3613cb92fb9b87a
paragambekar <69147632+paragambekar@users.noreply.github.com>
2022-05-31 10:58:33 +0530

    setup view engine

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ const app = express();
 const port = process.env.PORT;
 
 
+app.set('view engine', 'ejs');
+app.set('views' , './views');
+
+app.use('/', require('./routes/index'));
+
+
 app.listen(port, function(error){
     if(error){
         console.log(`Error in running server on port ${port}`);
